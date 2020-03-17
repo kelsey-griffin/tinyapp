6ea06211c04b9ac9a5fa45b372a63d54b042a00f
Kelsey Griffin <kjulegriffin@gmail.com>
2020-03-17 20:03:00 +0000

    fixed delete button that wasn't working. added update buttons and form.

--- express_server.js
+++ express_server.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
 const generateRandomString = () => {
-  return Math.random().toString(36).substring(7); 
+  return Math.random().toString(36).substring(7);
 };
 
 const urlDatabase = {
@@ -24,13 +24,13 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
+  const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
 app.post("/urls", (req, res) => {
-  let shortened = generateRandomString()
-  urlDatabase[shortened] = req.body.longURL
+  let shortened = generateRandomString();
+  urlDatabase[shortened] = req.body.longURL;
   res.redirect(`/urls/${shortened}`);
 });
 
@@ -38,7 +38,7 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
-  }
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -49,6 +49,11 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
+app.post("/urls/:shortURL", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL;
   res.redirect("/urls");
 });
 
