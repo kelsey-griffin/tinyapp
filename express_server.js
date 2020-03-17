c4c525753666de78e6e6a9a5094ec6a7cd55928e
Kelsey Griffin <kjulegriffin@gmail.com>
2020-03-17 02:56:29 +0000

    created generateRandomString function, created new submission form and post route to recieve form submission, installed body-parser.

--- express_server.js
+++ express_server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+const generateRandomString = () => {
+  return Math.random().toString(36).substring(7); 
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -11,6 +17,14 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello! This is my Tiny App");
+});
+
+app.post("/urls", (req, res) => {
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
